127a01cb78f2f72dc5425b4615b5783b415379da
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2020-09-09 13:56:38 +0200

    Use standard

--- test/parallel-destroy.js
+++ test/parallel-destroy.js
@@ -1,22 +1,22 @@
-var mouse = require('../');
+var mouse = require('../')
 
-var m1 = mouse();
-var m2 = mouse();
+var m1 = mouse()
+var m2 = mouse()
 
-m1.on('move', function(x, y) {
-	console.log('move_1', x, y);
-});
+m1.on('move', function (x, y) {
+  console.log('move_1', x, y)
+})
 
-m2.on('move', function(x, y) {
-	console.log('move_2', x, y);
-});
+m2.on('move', function (x, y) {
+  console.log('move_2', x, y)
+})
 
-setTimeout(function() {
-	m1.destroy();
-	console.log('destroy_1');
+setTimeout(function () {
+  m1.destroy()
+  console.log('destroy_1')
 
-	setTimeout(function() {
-		m2.destroy();
-		console.log('destroy_2');
-	}, 1000);
-}, 1000);
+  setTimeout(function () {
+    m2.destroy()
+    console.log('destroy_2')
+  }, 1000)
+}, 1000)
--- test/sequence-unref.js
+++ test/sequence-unref.js
@@ -1,23 +1,23 @@
-var mouse = require('../');
+var mouse = require('../')
 
-var m1 = mouse();
+var m1 = mouse()
 
-m1.on('move', function(x, y) {
-	console.log('move_1', x, y);
-});
+m1.on('move', function (x, y) {
+  console.log('move_1', x, y)
+})
 
-setTimeout(function() {
-	m1.unref();
-	console.log("unref_1");
+setTimeout(function () {
+  m1.unref()
+  console.log('unref_1')
 
-	var m2 = mouse();
+  var m2 = mouse()
 
-	m2.on('move', function(x, y) {
-		console.log('move_2', x, y);
-	});
+  m2.on('move', function (x, y) {
+    console.log('move_2', x, y)
+  })
 
-	setTimeout(function() {
-		m2.unref();
-		console.log("unref_2");
-	}, 1000);
-}, 1000);
+  setTimeout(function () {
+    m2.unref()
+    console.log('unref_2')
+  }, 1000)
+}, 1000)
